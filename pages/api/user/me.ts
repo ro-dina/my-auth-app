1f22c4a02ff949431cd485003c204a381a1c82fa
ro-dina <131938022+ro-dina@users.noreply.github.com>
2025-06-19 21:02:47 +0900

    リセットしたので動くところまで持ってきた

--- pages/api/user/me.ts
+++ pages/api/user/me.ts
@@ -18,7 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     const user = await prisma.user.findUnique({
-        where: { id: Number(payload.userId) },
+        where: { id: payload.userId },
         select: { id: true, email: true, createdAt: true },
     })
 
